5a6375eda8e38e868f010324c8211371e2d3430e
Robert Dennis <robertdennispersonal@gmail.com>
2020-11-06 19:59:23 -0500

    wip

---
+++ api/index.php
@@ -0,0 +1,5 @@
+<?php
+
+// This is the main entrypoint for functions on vercel, we don't use the traditional /public folder as the entrypoint for simplicity's sake.
+// You can read more here: https://calebporzio.com/easy-free-serverless-laravel-with-vercel
+require __DIR__ . '/../public/index.php';
