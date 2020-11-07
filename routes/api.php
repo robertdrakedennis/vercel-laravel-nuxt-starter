cdd39f10fdde6f7582b7f3d78c1fd4bdf02fc73b
Robert Dennis <robertdennispersonal@gmail.com>
2020-11-07 00:07:23 -0500

    wip

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/test', function (Request $request) {
+Route::get('/', function (Request $request) {
     return response()->json(['status' => 200]);
 });
